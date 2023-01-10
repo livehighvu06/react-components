84d66b7e552e260d45e521de825acf7045dffa7d
Eddie Chen <livehighvu06@gmail.com>
2023-01-10 18:25:20 +0800

    新增Table Component

--- src/pages/TablePage.js
+++ src/pages/TablePage.js
@@ -1,9 +1,34 @@
-import Table from "../components/Table";
+import SortableTable from "../components/SortableTable";
 
 function TablePage() {
+  const data = [
+    { name: "Orange", color: "bg-orange-500", score: 5 },
+    { name: "Apple", color: "bg-red-500", score: 3 },
+    { name: "Banana", color: "bg-yellow-500", score: 1 },
+    { name: "Lime", color: "bg-green-500", score: 4 },
+  ];
+  const config = [
+    {
+      label: "Name",
+      render: (fruit) => fruit.name,
+      sortValue: (fruit) => fruit.name,
+    },
+    {
+      label: "Color",
+      render: (fruit) => <div className={`p-3 m-2 ${fruit.color}`}></div>,
+    },
+    {
+      label: "Score",
+      render: (fruit) => fruit.score,
+      sortValue: (fruit) => fruit.score,
+    },
+  ];
+  const keyFn = (data) => {
+    return data.name;
+  };
   return (
     <div>
-      <Table />
+      <SortableTable data={data} config={config} keyFn={keyFn} />
     </div>
   );
 }
