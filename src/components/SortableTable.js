7c206b6efa22892b354ed2f64b63ad1259e1cd34
Eddie Chen <livehighvu06@gmail.com>
2023-01-10 18:55:15 +0800

    將SortableTable邏輯抽出，製作useSort Hook

--- src/components/SortableTable.js
+++ src/components/SortableTable.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import useSort from "../Hooks/use-sort";
 import Table from "./Table";
 import { GoArrowSmallDown, GoArrowSmallUp } from "react-icons/go";
 
@@ -25,29 +25,10 @@ const getIcons = (label, sortBy, sortOrder) => {
     return <GoArrowSmallDown />;
   }
 };
-function SortableTable(props) {
-  const [sortOrder, setSortOrder] = useState(null);
-  const [sortBy, setSortBy] = useState(null);
-  const { config, data } = props;
-  const handleClick = (label) => {
-    console.log(sortBy, label);
-    if (sortBy && label !== sortBy) {
-      setSortOrder("asc");
-      setSortBy(label);
-      return;
-    }
-    if (sortOrder === null) {
-      setSortOrder("asc");
-      setSortBy(label);
-    } else if (sortOrder === "asc") {
-      setSortOrder("desc");
-      setSortBy(label);
-    } else if (sortOrder === "desc") {
-      setSortOrder(null);
-      setSortBy(null);
-    }
-  };
 
+function SortableTable(props) {
+  const { config, data } = props;
+  const { sortOrder, sortBy, sortedData, handleClick } = useSort(config, data);
   const updatedConfig = config.map((column) => {
     if (!column.sortValue) return column;
     return {
@@ -65,21 +46,6 @@ function SortableTable(props) {
       ),
     };
   });
-
-  let sortedData = data;
-
-  if (sortOrder && sortBy) {
-    const { sortValue } = config.find((column) => column.label === sortBy);
-    sortedData = [...data].sort((a, b) => {
-      const valueA = sortValue(a);
-      const valueB = sortValue(b);
-      const reverseOrder = sortOrder === "asc" ? 1 : -1;
-      if (typeof valueA === "string") {
-        return valueA.localeCompare(valueB) * reverseOrder;
-      }
-      return (valueA - valueB) * reverseOrder;
-    });
-  }
 
   return <Table {...props} data={sortedData} config={updatedConfig} />;
 }
